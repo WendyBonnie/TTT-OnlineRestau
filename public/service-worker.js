3abe0bdd317090dad98a2f20d5e3a4c76da201db
wendy <wendymontagnon@gmail.com>
2021-03-22 16:38:21 +0100

    qr code

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1be85eb9740b82f5af60d2cae714f848.js"
+  "/precache-manifest.6821006aeb54189209f62b6ccd62f8a5.js"
 );
 
 self.addEventListener('message', (event) => {
